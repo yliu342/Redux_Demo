47cad489f3e545632630ec1a2f2789a056ad90de
YaKun Liu <yakun.liu@lightspeedhq.com>
2021-10-18 18:10:01 +1300

    Initial Commit

---
+++ Redux_Demo/Redux_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Redux_DemoApp.swift
+//  Redux_Demo
+//
+//  Created by Yakun Liu on 18/10/21.
+//
+
+import SwiftUI
+
+@main
+struct Redux_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
